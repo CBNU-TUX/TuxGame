9921ded212a963bbd30c8e8e2d02650baa9a19cf
songsuyoung <tndud3999@naver.com>
2022-08-23 16:10:07 +0900

    Opening

--- Assets/Source/Source/SpaceController.cs
+++ Assets/Source/Source/SpaceController.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class ComputerController : MonoBehaviour
+public class SpaceController : MonoBehaviour
 {
    [SerializeField]
-    GameObject MailUI;
+    GameObject Ui;
     
     void OnTriggerStay2D(Collider2D collision){
         if(Input.GetKey(KeyCode.Space)){
-            MailUI.SetActive(true);
+            Ui.SetActive(true);
         }
     }
 }
