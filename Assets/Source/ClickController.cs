66349466b79b89db44b69a64eec295093cb3f1c1
songsuyoung <tndud3999@naver.com>
2022-09-02 20:56:13 +0900

    Purchase materials Bugfix예정

--- Assets/Source/ClickController.cs
+++ Assets/Source/ClickController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using System;
 public class ClickController : MonoBehaviour
 {
 
@@ -17,27 +18,160 @@ public class ClickController : MonoBehaviour
     }
 
     public void OnClick(){
-        if(!isCheck&&!Input.GetKeyDown(KeyCode.Space)){
-            click = EventSystem.current.currentSelectedGameObject;
-            RectTransform clickTransform=click.GetComponent<RectTransform>();
-            clickTransform.anchoredPosition=new Vector2(clickTransform.anchoredPosition.x,clickTransform.anchoredPosition.y+30);
-            isCheck=true;
-            foreach(Button button in buttons){
-                if(click.name!=button.name){
-                    button.interactable = false;
+        GameObject child;
+        if(TotalGoldController.TotalGold>0){
+            if(!isCheck&&!Input.GetKeyDown(KeyCode.Space)){
+                click = EventSystem.current.currentSelectedGameObject;
+                child=click.transform.GetChild(1).gameObject.transform.GetChild(0).gameObject;
+                if(PlayerWorking.shovel==1||PlayerWorking.wateringCan==1){
+                    RectTransform clickTransform=click.GetComponent<RectTransform>();
+                    clickTransform.anchoredPosition=new Vector2(clickTransform.anchoredPosition.x,clickTransform.anchoredPosition.y+30);
+                    isCheck=true;
+                    foreach(Button button in buttons){
+                        if(click.name!=button.name){
+                            button.interactable = false;
+                        }
+                    }
+                }else if(PlayerWorking.fertillzer>0){
+                    RectTransform clickTransform=click.GetComponent<RectTransform>();
+                    clickTransform.anchoredPosition=new Vector2(clickTransform.anchoredPosition.x,clickTransform.anchoredPosition.y+30);
+                    isCheck=true;
+                    foreach(Button button in buttons){
+                        if(click.name!=button.name){
+                            button.interactable = false;
+                        }
+                    }
+                }else if(PlayerWorking.seed>0){
+                    
+                    RectTransform clickTransform=click.GetComponent<RectTransform>();
+                    clickTransform.anchoredPosition=new Vector2(clickTransform.anchoredPosition.x,clickTransform.anchoredPosition.y+30);
+                    isCheck=true;
+                    foreach(Button button in buttons){
+                        if(click.name!=button.name){
+                            button.interactable = false;
+                        }
+                    }
+                }else if(PlayerWorking.sprout>0){
+                    RectTransform clickTransform=click.GetComponent<RectTransform>();
+                    clickTransform.anchoredPosition=new Vector2(clickTransform.anchoredPosition.x,clickTransform.anchoredPosition.y+30);
+                    isCheck=true;
+                    foreach(Button button in buttons){
+                        if(click.name!=button.name){
+                            button.interactable = false;
+                        }
+                    }
+                }else if(PlayerWorking.sapling>0){
+
+                    RectTransform clickTransform=click.GetComponent<RectTransform>();
+                    clickTransform.anchoredPosition=new Vector2(clickTransform.anchoredPosition.x,clickTransform.anchoredPosition.y+30);
+                    isCheck=true;
+                    foreach(Button button in buttons){
+                        if(click.name!=button.name){
+                            button.interactable = false;
+                        }
+                    }
                 }
-            }
-        }else if(isCheck&&!Input.GetKeyDown(KeyCode.Space)){
-            click = EventSystem.current.currentSelectedGameObject;
-            RectTransform clickTransform=click.GetComponent<RectTransform>();
-            clickTransform.anchoredPosition=new Vector2(clickTransform.anchoredPosition.x,clickTransform.anchoredPosition.y-30);
-            isCheck=false;
-             foreach(Button button in buttons){
-                if(click.name!=button.name){
-                    button.interactable = true;
+
+            }else if(isCheck&&!Input.GetKeyDown(KeyCode.Space)){
+                click = EventSystem.current.currentSelectedGameObject;
+                RectTransform clickTransform=click.GetComponent<RectTransform>();
+                clickTransform.anchoredPosition=new Vector2(clickTransform.anchoredPosition.x,clickTransform.anchoredPosition.y-30);
+                isCheck=false;
+                foreach(Button button in buttons){
+                    if(click.name!=button.name){
+                        button.interactable = true;
+                    }
                 }
             }
         }
+    }
+    public void Buy(){
+        
+        bool isFirst=false;
+        click = EventSystem.current.currentSelectedGameObject;
+        GameObject child;
+        switch(click.name){
+            case "1":
+                child=GameObject.Find("BoxUI01").transform.GetChild(1).gameObject.transform.GetChild(0).gameObject;
+                break;
+            case "2":
+                child=GameObject.Find("BoxUI02").transform.GetChild(1).gameObject.transform.GetChild(0).gameObject;
+                break;
+            case "3":
+                child=GameObject.Find("BoxUI03").transform.GetChild(1).gameObject.transform.GetChild(0).gameObject;
+                break;
+            case "4":
+                child=GameObject.Find("BoxUI04").transform.GetChild(1).gameObject.transform.GetChild(0).gameObject;
+                break;
+            case "5":
+                child=GameObject.Find("BoxUI05").transform.GetChild(1).gameObject.transform.GetChild(0).gameObject;
+                break;
+            default:
+                child=GameObject.Find("BoxUI06").transform.GetChild(1).gameObject.transform.GetChild(0).gameObject;
+                break;
+
+        }
+        
+        try{
+            if(!isFirst){
+
+                string price=click.transform.GetChild(0).gameObject.GetComponent<Text>().text;
+                Debug.Log(click.name+" "+price);
+                if(TotalGoldController.TotalGold>0){
+
+                    if(price.Contains("50")){
+                            
+                        TotalGoldController.TotalGold-=50;
+                        if(TotalGoldController.TotalGold<0){
+                            TotalGoldController.TotalGold+=50;
+                            return ;
+                        }
+                        if(click.name=="1"){
+                            PlayerWorking.shovel=1;
+                            child.GetComponent<Text>().text=PlayerWorking.shovel.ToString()+" 개";
+                        }else if(click.name=="3"){
+                            PlayerWorking.wateringCan=1;
+                            child.GetComponent<Text>().text=PlayerWorking.wateringCan.ToString()+" 개";
+                        }else if(click.name=="4"){
+                            PlayerWorking.seed+=1;
+                            child.GetComponent<Text>().text=PlayerWorking.seed.ToString()+" 개";
+                        }
+
+                    }else if(price.Contains("100")){
+
+                        TotalGoldController.TotalGold-=100;
+                        
+                        if(TotalGoldController.TotalGold<0){
+                            TotalGoldController.TotalGold+=100;
+                            return;
+                        }
+
+                        if(click.name=="2"){                           
+                            PlayerWorking.fertillzer+=1;
+                            child.GetComponent<Text>().text=PlayerWorking.fertillzer.ToString()+" 개";
+                        }else{
+                            PlayerWorking.sprout+=1;
+                            child.GetComponent<Text>().text=PlayerWorking.sprout.ToString()+" 개";
+                        }
+                    
+
+                    }else if(price.Contains("300")){
+                        PlayerWorking.sapling+=1;
+                        TotalGoldController.TotalGold-=300;
+                        
+                        if(TotalGoldController.TotalGold<0){
+                            TotalGoldController.TotalGold+=300;
+                            PlayerWorking.sapling-=1;
+                        }
+                        child.GetComponent<Text>().text=PlayerWorking.sapling.ToString()+" 개";
+                    }
+                }
+                isFirst=true;
+            }
+        }catch(NullReferenceException ex){
+            ;
+        }
+        
     }
     // Start is called before the first frame update
    
--- Assets/Source/PlayerWorking.cs
+++ Assets/Source/PlayerWorking.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 public class PlayerWorking : MonoBehaviour
 {
     static public List<SoilInfo> working;
    
+    static public int shovel;
+    static public int fertillzer;
+    static public int wateringCan;
+    static public int seed;
+    static public int sprout;
+    static public int sapling;
+
+    
+
     // Start is called before the first frame update
     ClickController clickObject;
     void Start(){
@@ -14,14 +24,14 @@ public class PlayerWorking : MonoBehaviour
     }
 
     void OnTriggerStay2D(Collider2D collision){
+        
         try{
             if(clickObject==null)
                 clickObject = GameObject.FindObjectOfType<ClickController>();
-
+            GameObject Tchild=clickObject.transform.GetChild(1).gameObject.transform.GetChild(0).gameObject;
             SoilInfo tmp=collision.gameObject.GetComponent<SoilInfo>();
-            if(clickObject.click.name=="BoxUI01"&&clickObject.click!=null&&tmp.level<1)
+            if(clickObject.click.name=="BoxUI01"&&clickObject.click!=null&&tmp.level<1&&shovel>0)
             {
-
                 if(Input.GetKey(KeyCode.Space)){
                     tmp.treelevel="No";
                     tmp.level=1;
@@ -30,22 +40,27 @@ public class PlayerWorking : MonoBehaviour
                     this.gameObject.GetComponent<Animator>().SetTrigger("isDigging");
                 }
             }
-            else if(clickObject.click.name=="BoxUI03"&&clickObject.click!=null){
+            else if(clickObject.click.name=="BoxUI03"&&clickObject.click!=null&&wateringCan>0){
                 if(Input.GetKey(KeyCode.Space)){
                     collision.gameObject.GetComponent<Animator>().SetTrigger("isStarting");
                     this.gameObject.GetComponent<Animator>().SetTrigger("isWater");
                 }
-            }else if(clickObject.click.name=="BoxUI04"&&clickObject.click!=null&&tmp.level>=1&&tmp.treelevel=="No"){
+            }else if(clickObject.click.name=="BoxUI04"&&clickObject.click!=null&&tmp.level>=1&&tmp.treelevel=="No"&&seed>0){
                 if(Input.GetKey(KeyCode.Space)){
                     tmp.isGrowing=true;
+                    seed-=1;
+                    Tchild.GetComponent<Text>().text=PlayerWorking.seed.ToString();
                     Debug.Log(collision.gameObject.name);
                     tmp.treelevel="seed";
                     GameObject child=collision.gameObject.transform.Find("seed").gameObject;
                     Debug.Log(child.name);
                     child.gameObject.SetActive(true);
                 }
-            }else if(clickObject.click.name=="BoxUI05"&&clickObject.click!=null&&tmp.level>=1&&tmp.treelevel=="No"){
+            }else if(clickObject.click.name=="BoxUI05"&&clickObject.click!=null&&tmp.level>=1&&tmp.treelevel=="No"&&sprout>0){
                 if(Input.GetKey(KeyCode.Space)){
+                    sprout-=1;
+                    
+                    Tchild.GetComponent<Text>().text=PlayerWorking.sprout.ToString();
                     tmp.isGrowing=true;
                     Debug.Log(collision.gameObject.name);
                     tmp.treelevel="sprout";
@@ -53,9 +68,10 @@ public class PlayerWorking : MonoBehaviour
                     Debug.Log(child.name);
                     child.gameObject.SetActive(true);
                 }
-            }else if(clickObject.click.name=="BoxUI06"&&clickObject.click!=null&&tmp.level>=1&&tmp.treelevel=="No"){
+            }else if(clickObject.click.name=="BoxUI06"&&clickObject.click!=null&&tmp.level>=1&&tmp.treelevel=="No"&&sapling>0){
                 if(Input.GetKey(KeyCode.Space)){
-                   
+                    sapling-=1;
+                    Tchild.GetComponent<Text>().text=PlayerWorking.sapling.ToString();
                     Debug.Log(collision.gameObject.name);
                     tmp.treelevel="tree";
                     GameObject child=collision.gameObject.transform.Find("tree").gameObject;
